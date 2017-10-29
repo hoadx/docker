266ed72fe967a1662c360fc331c5fed201daff36
HoaDX <duongxuanhoa@gmail.com>
2017-10-30 01:21:20 +0700

    Update Nginx container

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -1,19 +1,15 @@
 #!/bin/sh
 set -e
 
-if test $USER_ID -gt 0
+if test $CHANGE_OWNER -gt 0
 then                  
-        ORIGPASSWD=$(cat /etc/passwd | grep www)
+        ORIGPASSWD=$(cat /etc/passwd | grep $USERNAME)
         ORIG_UID=$(echo $ORIGPASSWD | cut -f3 -d:)
         ORIG_GID=$(echo $ORIGPASSWD | cut -f4 -d:)
-                                                  
-        USER_ID=${USER_ID:=$ORIG_UID}
-        GROUP_ID=${GROUP_ID:=$ORIG_GID}
-                                       
+                                                                                
         if [ \("$USER_ID" != "$ORIG_UID"\) -o \("$GROUP_ID" != "$ORIG_GID"\) ]; then
-        echo "go here"                                                      
                 sed -i -e "s/:$ORIG_UID:$ORIG_GID:/:$USER_ID:$GROUP_ID:/" /etc/passwd
-                sed -i -e "s/www:x:$ORIG_GID:/www:x:$GROUP_ID:/" /etc/group          
+                sed -i -e "s/$USERNAME:x:$ORIG_GID:/$USERNAME:x:$GROUP_ID:/" /etc/group          
                 ORIG_HOME=$(echo $ORIGPASSWD | cut -f6 -d:)                
                 chown -R ${USER_ID}:${GROUP_ID} ${ORIG_HOME}
         fi                                                  
